418e0ab4f10af288f25395068dd56159d06b0364
Back to the future <shrikant.swami@gmail.com>
2019-10-29 22:07:48 +0530

    Add files via upload

---
+++ student1/EX08_SelectDriver.java
@@ -0,0 +1,40 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class EX08_SelectDriver
+{
+    public static WebDriver driver = null;
+
+    void selectDriver(String driverName)
+    {
+        switch(driverName)
+        {
+            case "chrome":
+                System.setProperty("webdriver.chrome.driver","browser_driver\\chromedriver.exe");
+                driver = new ChromeDriver();
+                break;
+
+            case "firefox":
+                System.setProperty("webdriver.firefox.marionette","browser_driver\\geckodriver.exe");
+                driver = new FirefoxDriver();
+                break;
+            default :
+                System.out.println("not valid drivername");
+                driver=null;
+                break;
+        }
+    }
+    public static void main(String[] args)
+    {
+        EX08_SelectDriver s = new EX08_SelectDriver();
+        for(int nCnt =0; nCnt < 3; nCnt++){
+            try{s.selectDriver("chrome");}
+            catch(Exception e){e.printStackTrace();}
+        }
+        driver.get("https://www.google.com");
+
+
+
+    }
+}
