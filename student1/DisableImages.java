5926a44061fe2eaa39ba56fa03fc8b744e8eb947
Back to the future <shrikant.swami@gmail.com>
2019-10-29 22:05:37 +0530

    Add files via upload

---
+++ student1/DisableImages.java
@@ -0,0 +1,25 @@
+package com.seleniumpractice;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class DisableImages
+{
+    public static void main(String[] args) throws InterruptedException {
+
+//        System.setProperty("webdriver.chrome.driver","browser_driver\\chromedriver.exe");
+//        ChromeOptions options = new ChromeOptions();
+//        options.AddUserProfilePreference("profile.default_content_setting_values.images", 2);
+//        driver = new ChromeDriver(options);
+//        driver.get("http://automationpractice.com/index.php");
+//        Thread.sleep(2000);
+//
+//
+//
+//
+//        Thread.sleep(2000);
+//        driver.close();
+//        driver.quit();
+    }
+}
